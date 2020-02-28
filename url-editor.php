ded7743996ba778992eff2d6c502485633c6a48a
bahax41 <bahax41@gmail.com>
2020-02-28 14:39:55 +0300

    Update url-editor.php

--- url-editor.php
+++ url-editor.php
@@ -1,7 +1,7 @@
 <?php
 	$input_lines = "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda https://www.bahadirduzcan.com/ pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.";
 	
-	preg_match_all('/(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&\'\(\)\*\+,;=.]+/', $postTextDetails, $output_array);
+	preg_match_all('/(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&\'\(\)\*\+,;=.]+/', $input_lines, $output_array);
 	
 	foreach ($output_array[0] as $deger) {
 		echo "&nbsp;<a href=\"$deger\">LİNK</a>&nbsp;";
